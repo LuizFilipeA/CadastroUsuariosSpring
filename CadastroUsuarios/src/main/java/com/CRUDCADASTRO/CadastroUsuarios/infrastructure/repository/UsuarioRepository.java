65c4fc59d3f93db17aa2b43122b5ca635555b21b
LuizFilipeA <luizfilipealvessantos@outlook.com>
2025-12-02 06:28:16 -0300

    Repository Usuario - New method findByEmail

--- CadastroUsuarios/src/main/java/com/CRUDCADASTRO/CadastroUsuarios/infrastructure/repository/UsuarioRepository.java
+++ CadastroUsuarios/src/main/java/com/CRUDCADASTRO/CadastroUsuarios/infrastructure/repository/UsuarioRepository.java
@@ -3,5 +3,11 @@ package com.CRUDCADASTRO.CadastroUsuarios.infrastructure.repository;
 import com.CRUDCADASTRO.CadastroUsuarios.infrastructure.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository <Usuario, Integer> {
+
+    //findBy por padrao, "Email" tem que corresponder a uma coluna da tabela de usuarios e obrigatoriamente com a primeira letra em maiusculo = findByEmail
+   Optional<Usuario> findByEmail(String email);
 }
