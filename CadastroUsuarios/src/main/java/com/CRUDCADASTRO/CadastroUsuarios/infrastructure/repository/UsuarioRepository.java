abd1b1ea119584f4c6281dfb2c0f623abb25ac6c
LuizFilipeA <luizfilipealvessantos@outlook.com>
2025-12-03 23:10:38 -0300

    Repository Usuario - New method deleteByEmail

--- CadastroUsuarios/src/main/java/com/CRUDCADASTRO/CadastroUsuarios/infrastructure/repository/UsuarioRepository.java
+++ CadastroUsuarios/src/main/java/com/CRUDCADASTRO/CadastroUsuarios/infrastructure/repository/UsuarioRepository.java
@@ -1,13 +1,16 @@
 package com.CRUDCADASTRO.CadastroUsuarios.infrastructure.repository;
 
 import com.CRUDCADASTRO.CadastroUsuarios.infrastructure.entity.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Integer> {
 
     //findBy por padrao, "Email" tem que corresponder a uma coluna da tabela de usuarios e obrigatoriamente com a primeira letra em maiusculo = findByEmail
    Optional<Usuario> findByEmail(String email);
+
+   @Transactional //Caso ocorra algum erro, essa anotação não permite que seja deletado
+   void deleteByEmail (String email);
 }
