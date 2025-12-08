ca6225943a90fc411ca3715173931f4333ebd9d5
LuizFilipeA <luizfilipealvessantos@outlook.com>
2025-12-08 12:18:01 -0300

    Criação da classe controller de usuarios

---
+++ CadastroUsuarios/src/main/java/com/CRUDCADASTRO/CadastroUsuarios/controller/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.CRUDCADASTRO.CadastroUsuarios.controller;
+
+import com.CRUDCADASTRO.CadastroUsuarios.business.usuarioService.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+}
